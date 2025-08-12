22ad63d32fef4b8fb855cfad607ceffc7f3ea04c
Rogul-Jayaraman <jayaramanrogul@gmail.com>
2025-08-13 00:43:13 +0530

    deleteing project forign key issue solved

--- app/api/project/deleteProject/route.js
+++ app/api/project/deleteProject/route.js
@@ -1,34 +1,52 @@
 import db from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-export async function DELETE(req){
-    try{
-        const {id} = await req.json();
-        if(!id){
-            return new Response(JSON.stringify({message:"Server Error"}),{status:500})
-        }
-        
-        const {userId} = await auth();
-        if(!userId){
-            return new Response(JSON.stringify({message:"Unauthorized"}),{status:401})
-        }
-
-        const user = await db.user.findUnique({
-            where : {
-                clerkUserId : userId
-            }
-        })
-        if(!user){
-            return new Response(JSON.stringify({message:"User not found"}),{status:404})
-        }
-        await db.project.delete({
-            where:{
-                id
-            }
-        })
-        return new Response(JSON.stringify({message:"Success"}),{status:200})
-    }catch(err){
-        console.log("Error in deleting the project",err.message);
-        return new Response(JSON.stringify({message:"Error while deleting the project"}),{status:500})
+export async function DELETE(req) {
+  try {
+    const { id } = await req.json();
+    if (!id) {
+      return new Response(JSON.stringify({ message: "Server Error" }), {
+        status: 500,
+      });
     }
+
+    const { userId } = await auth();
+    if (!userId) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+    });
+    if (!user) {
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+    await prisma.task.deleteMany({
+      where: {
+        projectId: projectIdToDelete,
+      },
+    });
+
+    await prisma.project.delete({
+      where: {
+        id: projectIdToDelete,
+      },
+    });
+
+    return new Response(JSON.stringify({ message: "Success" }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log("Error in deleting the project", err.message);
+    return new Response(
+      JSON.stringify({ message: "Error while deleting the project" }),
+      { status: 500 }
+    );
+  }
 }
